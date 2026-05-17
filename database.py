37d5f08c717f44c885782c160b7fe1a8e2ce08fa
zing <zing@gmail.com>
2026-05-17 10:53:59 +0800

    完成查詢用api

--- database.py
+++ database.py
@@ -11,7 +11,72 @@ from dotenv import load_dotenv
 load_dotenv()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def get_data_by_county(county):
+    conn, cursor = open_db()
+    result = {"success": True, "message": None,"rows": None}
 
+    if not conn:
+        result["success"] = False
+        result["message"] = "資料庫開啟失敗"
+
+        return result
+    sql="""select * from data where county=%s
+    and datacreationdate=
+    (select max(datacreationdate) from data);
+    """
+    try:
+        cursor.execute(sql,(county,))
+
+        # 取得資料欄位名稱
+        rows = cursor.fetchall()
+        result["success"] = True
+        result["rows"] = rows
+
+        return result
+    except Exception as e:
+        result["success"] = False
+        result["message"] = f"資料庫查詢失敗:{e}"
+
+        return result
+    finally:
+        conn.close()
+
+
+
+
+#取得不重複縣市
+def get_counties():
+    conn, cursor = open_db()
+    result = {"success": True, "message": None,"rows": None}
+
+    if not conn:
+        result["success"] = False
+        result["message"] = "資料庫開啟失敗"
+
+        return result
+    sql="select DISTINCT county from data ORDER by county desc;"
+    try:
+        cursor.execute(sql)
+
+        # 取得資料欄位名稱
+        rows = cursor.fetchall()
+        result["success"] = True
+        result["rows"] = rows
+
+        return result
+    except Exception as e:
+        result["success"] = False
+        result["message"] = f"資料庫查詢失敗:{e}"
+
+        return result
+    finally:
+        conn.close()
+
+
+
+
+
+#取得最新資料
 def get_latest_data():
     conn, cursor = open_db()
     result = {"success": True, "message": None, "columns": None, "rows": None}
@@ -68,5 +133,5 @@ def open_db():
 
     return None, None
 
-
-print(get_latest_data())
+if __name__=="main":
+    print(get_data_by_county("county"))
